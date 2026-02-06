b77358eda97c85d7fe49fb5ed67a2cb8d297bd9c
DistantCandle <loftuswp2@gmail.com>
2026-02-06 04:59:46 -0500

    bugfix for previous commit

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -4,23 +4,23 @@ import unittest
 class TestEmotionDetector(unittest.TestCase):
     def test_emotion_detector(self):
         # Test case for joy
-        result_1 = emotion('I am glad this happened')
-        self.assertEqual(result_1['label'], 'joy')
+        result_1 = emotion_detector('I am glad this happened')
+        self.assertEqual(result_1['dominant_emotion'], 'joy')
 
         # Test case for anger
-        result_2 = emotion('I am really mad about this')
-        self.assertEqual(result_2['label'], 'anger')
+        result_2 = emotion_detector('I am really mad about this')
+        self.assertEqual(result_2['dominant_emotion'], 'anger')
 
         # Test case for disgust
-        result_3 = emotion('I feel disgusted just hearing about this')
-        self.assertEqual(result_3['label'], 'disgust')
+        result_3 = emotion_detector('I feel disgusted just hearing about this')
+        self.assertEqual(result_3['dominant_emotion'], 'disgust')
 
         # Test case for sadness
-        result_4 = emotion('I am so sad about this')
-        self.assertEqual(result_4['label'], 'sadness')
+        result_4 = emotion_detector('I am so sad about this')
+        self.assertEqual(result_4['dominant_emotion'], 'sadness')
 
         # Test case for fear
-        result_5 = emotion('I am really afraid that this will happen')
-        self.assertEqual(result_5['label'], 'fear')
+        result_5 = emotion_detector('I am really afraid that this will happen')
+        self.assertEqual(result_5['dominant_emotion'], 'fear')
 
 unittest.main()
