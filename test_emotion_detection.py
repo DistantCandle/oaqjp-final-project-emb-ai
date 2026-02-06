c9ad09f96b512d81a4b0e9b0b44686176c579bcf
DistantCandle <loftuswp2@gmail.com>
2026-02-06 04:58:10 -0500

    bugfix for previous commit

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -4,23 +4,23 @@ import unittest
 class TestEmotionDetector(unittest.TestCase):
     def test_emotion_detector(self):
         # Test case for joy
-        result_1 = sentiment_analyzer('I am glad this happened')
+        result_1 = emotion('I am glad this happened')
         self.assertEqual(result_1['label'], 'joy')
 
         # Test case for anger
-        result_2 = sentiment_analyzer('I am really mad about this')
+        result_2 = emotion('I am really mad about this')
         self.assertEqual(result_2['label'], 'anger')
 
         # Test case for disgust
-        result_3 = sentiment_analyzer('I feel disgusted just hearing about this')
+        result_3 = emotion('I feel disgusted just hearing about this')
         self.assertEqual(result_3['label'], 'disgust')
 
         # Test case for sadness
-        result_4 = sentiment_analyzer('I am so sad about this')
+        result_4 = emotion('I am so sad about this')
         self.assertEqual(result_4['label'], 'sadness')
 
         # Test case for fear
-        result_5 = sentiment_analyzer('I am really afraid that this will happen')
+        result_5 = emotion('I am really afraid that this will happen')
         self.assertEqual(result_5['label'], 'fear')
 
 unittest.main()
