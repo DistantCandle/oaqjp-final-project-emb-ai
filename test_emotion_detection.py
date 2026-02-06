2aac80c1e89303e7e2a75db9f3883265a58ea63d
DistantCandle <loftuswp2@gmail.com>
2026-02-06 04:56:37 -0500

    bugfix for previous commit

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,4 +1,4 @@
-from EmotionDetectionemotion_detection import emotion_detector
+from EmotionDetection.emotion_detection import emotion_detector
 import unittest
 
 class TestEmotionDetector(unittest.TestCase):
